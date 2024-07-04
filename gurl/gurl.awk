53a00ddb7780ca82a56ef266a9878764bfa1964e
cauri <43008483+c-auri@users.noreply.github.com>
2024-07-05 00:01:35 +0200

    add gurl

---
+++ gurl/gurl.awk
@@ -0,0 +1,12 @@
+/^\s*$/ {
+    current_table = ""
+}
+/^\[.*\]$/ {
+    current_table = substr($0, 2, length - 2)
+}
+current_table == table && $1 ~ key {
+    result = $2
+}
+END {
+    print result
+}
---
+++ gurl/gurl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# == 0 ]]; then
+    echo "Error: no arguments provided."
+    exit 2
+elif [[ $# == 1 ]]; then
+    table="";
+    key=$1;
+elif [[ $# == 2 ]]; then
+    table=$1;
+    key=$2
+else
+    echo "Error: too many arguments provided."
+    exit 2;
+fi
+
+awk -v table="$table" -v key="$key" -f /usr/local/bin/gurl/gurl.awk ~/notes/urls
+
+exit 0
